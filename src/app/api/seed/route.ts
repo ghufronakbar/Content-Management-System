50dea19cd5d5f2ef604aa4221e5a75b27413678a
ghufron akbar <alan.040219.gam@gmail.com>
2025-03-18 22:01:36 +0700

    Remove Seed

--- src/app/api/seed/route.ts
+++ src/app/api/seed/route.ts
@@ -1,20 +1,5 @@
 import { NextResponse } from "next/server";
-import prisma from "~/config/prisma";
 
 export const GET = async () => {
-  return NextResponse.json("Hello World");
-  const features = await prisma.feature.findMany({
-    select: {
-      title: true,
-      content: true,
-      image: true,
-      subtitle: true,
-    },
-  });
-
-  await prisma.feature.createMany({
-    data: features,
-  });
-
-  return NextResponse.json("Successfull to create features");
+  return NextResponse.json("Hello");
 };
